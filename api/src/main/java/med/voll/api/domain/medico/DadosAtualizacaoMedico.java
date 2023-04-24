d27b9edfb209c549ad1740234ea450852fc5fbc8
Julio César <juliocesarcs2004@gmail.com>
2023-04-24 20:50:42 -0300

    Implementado o tratamento de exeçcoes de forma limpa utilizando o Spring Boot

--- api/src/main/java/med/voll/api/domain/medico/DadosAtualizacaoMedico.java
+++ api/src/main/java/med/voll/api/domain/medico/DadosAtualizacaoMedico.java
@@ -1,14 +1,12 @@
-package med.voll.api.paciente;
+package med.voll.api.domain.medico;
 
-import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotNull;
-import med.voll.api.endereco.DadosEndereco;
+import med.voll.api.domain.endereco.DadosEndereco;
 
-public record DadosAtualizacaoPaciente(
+public record DadosAtualizacaoMedico(
         @NotNull
         Long id,
         String nome,
         String telefone,
-        @Valid
         DadosEndereco endereco) {
 }
---
+++ api/src/main/java/med/voll/api/domain/paciente/DadosAtualizacaoPaciente.java
@@ -0,0 +1,14 @@
+package med.voll.api.domain.paciente;
+
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
+import med.voll.api.domain.endereco.DadosEndereco;
+
+public record DadosAtualizacaoPaciente(
+        @NotNull
+        Long id,
+        String nome,
+        String telefone,
+        @Valid
+        DadosEndereco endereco) {
+}
