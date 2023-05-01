2756bd3116693b484602a3d5ff01bb60778dc70e
Julio César <juliocesarcs2004@gmail.com>
2023-05-01 11:54:36 -0300

    Implementando validacao autenticacao e controle de acesso na API

--- api/src/main/java/med/voll/api/infra/security/TokenService.java
+++ api/src/main/java/med/voll/api/infra/security/TokenService.java
@@ -3,6 +3,7 @@ package med.voll.api.infra.security;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
 import med.voll.api.domain.usuario.Usuario;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -10,7 +11,6 @@ import org.springframework.stereotype.Service;
 import java.time.Instant;
 import java.time.LocalDateTime;
 import java.time.ZoneOffset;
-import java.util.Date;
 
 @Service
 public class TokenService {
@@ -29,6 +29,21 @@ public class TokenService {
         } catch (JWTCreationException exception){
             throw  new RuntimeException("erro ao gerar token jwt", exception);
         }
+    }
+
+    public String getSubject(String tokenJWT) {
+        try {
+            var algoritmo = Algorithm.HMAC256(secret);
+            return JWT.require(algoritmo)
+                    .withIssuer("API Voll.med")
+                    .build()
+                    .verify(tokenJWT)
+                    .getSubject();
+        } catch (JWTVerificationException exception) {
+            throw new RuntimeException("Token JWT inválido ou expirado!");
+        }
+
+
     }
 
     private Instant dataExpiracao() {
